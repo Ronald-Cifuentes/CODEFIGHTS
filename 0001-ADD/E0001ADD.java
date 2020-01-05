368874bf9fb9f8ce7c65a1b4b7613599cbb1caeb
r <alexis1991.rc@gmail.com>
2020-01-05 11:35:23 -0500

    Primer commit - Suma de dos numeros

---
+++ 0001-ADD/E0001ADD.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class E0001ADD {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,add(1,2));
+    }
+    
+    public static int add(int par1, int par2){
+        return par1+par2;
+    }
+    
+}
