5f31127b378e1778c5cdc1ba6184cd4855d98b86
qubixxx <alexis1991.rc@gmail.com>
2020-01-05 13:04:21 -0500

    Ultima Correccion primer ejercicio

--- 0001-ADD/ADD.java
+++ 0001-ADD/ADD.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class ADD {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,add(1,2));
+        JOptionPane.showMessageDialog(null,"La suma de 1 y 2 es: "+add(1,2));
     }
     
     public static int add(int par1, int par2){
